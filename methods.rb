17a52df72289a7df0cb90837d1cd0d4c48b15a0f
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-05-16 11:48:39 -0400

    commit tree 17d343e7b9573aa520956fece598075878530190

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts 'Hello, programmer!'
+  end
+
+def greet(string)
+    puts "Hello, #{string}!" 
+end
+
+def greet_with_default(string = 'programmer')
+    puts "Hello, #{string}!"
+end
+
+def add(num1,num2)
+    num1+num2
+end
+
+def halve(int)
+    if int.class != Integer
+        nil
+    else int/2
+    end
+end
